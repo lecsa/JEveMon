68d883cc90ef4c02c35157b2e3663c739ea07fc0
Bayi <bayi@bayi.hu>
2013-09-06 01:13:47 +0200

    Example save and load function for Settings class

--- JEveMon/src/main/java/settings/Settings.java
+++ JEveMon/src/main/java/settings/Settings.java
@@ -5,9 +5,13 @@
 package settings;
 
 import java.io.Externalizable;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInput;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
 
 /**
  *
@@ -20,14 +24,47 @@ public class Settings implements Externalizable{
      */
     public transient static boolean isDebug = true;
 
-    
     @Override
     public void writeExternal(ObjectOutput oo) throws IOException {
-        oo.writeObject(isDebug);
+        oo.writeObject(Settings.isDebug);
     }
 
     @Override
     public void readExternal(ObjectInput oi) throws IOException, ClassNotFoundException {
-        isDebug = (boolean) oi.readObject();
+        Settings.isDebug = (boolean) oi.readObject();
+    }
+    
+    /** 
+     * Saves Settings to fileName
+     * @param fileName File Name ( with path ) to save to
+     * @return true on success, false on fail
+     */
+    public static boolean save(String fileName) {
+        Settings instance = new Settings();
+        if ( Settings.isDebug ) System.out.println("Saving Settings ... (File Name: " + fileName + ")");
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream( fileName ))) {
+            oos.writeObject(instance);
+        } catch (Exception ex ) {
+            if ( Settings.isDebug ) System.out.println("Error saving Settings: ");
+            if ( Settings.isDebug ) ex.printStackTrace(System.out);
+            return false;                
+        }
+        return true;
+    }
+    
+    /**
+     * Loades Settings from fileName
+     * @param fileName File Name ( with path ) to load from
+     * @return true on success, false on fail
+     */
+    public static boolean load(String fileName) {
+        try ( ObjectInputStream ois = new ObjectInputStream(new FileInputStream( fileName )) ) {
+            Settings instance = (Settings) ois.readObject();
+        } catch( Exception ex ) {
+            if ( Settings.isDebug ) System.out.println("Error loading Settings: ");
+            if ( Settings.isDebug ) ex.printStackTrace(System.out);
+            return false;
+        }
+        return true;
     }
 }
